c929881db22b5072ba21523987b69752e3721ef2
twinaphex <libretro@gmail.com>
2015-10-14 05:57:44 +0200

    Remove more not needed in native/

--- native/android/app-android.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "input/keycodes.h"
-
-// Compatability we alias the keycodes
-// since native's keycodes are based on
-// android keycodes.
-typedef enum _keycode_t AndroidKeyCodes;
--- native/android/native-audio-so.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef int (*AndroidAudioCallback)(short *buffer, int num_samples);
-
-bool OpenSLWrap_Init(AndroidAudioCallback cb, int _FramesPerBuffer, int _SampleRate);
-void OpenSLWrap_Shutdown();
